e71ad9c4f760accc0065bb96091795f26d0b5242
Cindy <cindyakumu40@gmail.com>
2024-07-11 12:11:41 +0300

    declaring variables

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-const companyName ='is set as Scruber';
+const companyName ='Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
